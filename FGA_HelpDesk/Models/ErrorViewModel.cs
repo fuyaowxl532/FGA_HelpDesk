588f78cc9d0d1f80568837aebbf8c27f5773c936
xwang6-a <xwang6-a@DA-E-0080>
2022-08-08 14:00:04 -0400

    Add project files.

---
+++ FGA_HelpDesk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FGA_HelpDesk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
